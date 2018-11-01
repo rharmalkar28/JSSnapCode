3bd537610034993d717709acd1b012ad4a99ca2e
Rahul Harmalkar <rharmalkar28@hotmail.com>
2018-11-01 15:53:33 +0530

    example added

--- functionGeneratorEx.js
+++ functionGeneratorEx.js
@@ -18,3 +18,6 @@ function* range(arrey1, arrey2) {
 for (let i of range([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [3, 3, 4])) {
   console.log(i)
 }
+
+var item = range([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [3, 3, 4]);
+item.next();
