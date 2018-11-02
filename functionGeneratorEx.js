342005bec19562c37a41e2eb33b0083fad8e71f6
Rahul Harmalkar <rharmalkar28@hotmail.com>
2018-11-02 13:16:22 +0530

    fix bugs on some input

--- functionGeneratorEx.js
+++ functionGeneratorEx.js
@@ -1,9 +1,9 @@
-function* range(arrey1, arrey2) {
+function* arreyIterator(arrey1, arrey2) {
   let returnVal = '';
   let pos = 1;
   let nextOutIndex = 0;
   for (let i of arrey1) {
-    returnVal += '' + arrey1[i];
+    returnVal += i.toString();
     if (arrey2[nextOutIndex] == pos) {
       yield returnVal;
       pos = 1;
@@ -15,9 +15,25 @@ function* range(arrey1, arrey2) {
   }
 }
 
-for (let i of range([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [3, 3, 4])) {
+const formatMobileNo = number => {
+  const numberArrey = number.toString().split('');
+  if (numberArrey.length === 10) {
+    const item = arreyIterator(numberArrey, [3, 3, 4]);
+    return `(${item.next().value}) ${item.next().value}-${item.next().value}`;
+  }
+  return number;
+};
+
+const unFormatMobileNo = formatedMobile => {
+  return formatedMobile.match(/[0-9]/g).join('');
+};
+
+for (let i of arreyIterator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [3, 3, 4])) {
   console.log(i)
 }
 
-var item = range([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [3, 3, 4]);
+var item = arreyIterator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [3, 3, 4]);
 item.next();
+
+// a liabrary for mobile numbers
+// https://github.com/catamphetamine/libphonenumber-js
